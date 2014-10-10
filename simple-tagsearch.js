356763c1143b95a5c0453d92092b46e466984f6d
Björn Henriksson <bjarna2@gmail.com>
2014-10-10 12:16:07 +0200

    Create simple-tagsearch.js

---
+++ simple-tagsearch.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function ($) {
+
+	$( ".searchitem" ).hide(); //this hides all items in list on page load
+	$( ".searchitem span" ).hide(); //hides all the search tags in your <span> tag
+
+	$("#searchthis").on("keyup", function(e) { //activates if user types in the input box
+
+		var value = $( "#searchthis" ).val(); //fetches what the user is typing in the input box
+		var length = $( "#searchthis" ).val().length; //counts characters currently in input box
+
+		if (length > 1) { //if input box has more that 2 characters
+    		$( ".searchitem:contains('" + value + "')").show(); //show results, activates the class that relates to what the user searches for
+    	}
+
+    	if(e.keyCode == 8) {
+    		$( ".searchitem" ).hide(); //hides results if backspace is pressed
+    	}
+
+    	if(e.keyCode == 16) { //ignores shift key
+    		return;
+    	}
+	});	
+
+}); 
